7bdcd2aafbfcc26cd46f64ad60a84ccf99e4df75
Jason Yonathan Huang <jasonyonathan@live.com>
2020-10-01 14:05:33 +0700

    Update check-login.php
    added htmlspecialchars for extended security, trim to trim whitespaces

--- user/login/check-login.php
+++ user/login/check-login.php
@@ -14,8 +14,8 @@ if ( isset($_POST['username']) && isset($_POST['password']) ) {
     $check_log = $dbconnect->prepare($sql_check);
     $check_log->bind_param('ss', $username, $password);
 
-    $username = $_POST['username'];
-    $password = md5( $_POST['password'] );
+    $username = htmlspecialchars(trim($_POST['username']));
+    $password = md5( trim($_POST['password']) );
 
     $check_log->execute();
 
